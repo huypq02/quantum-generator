65b5d4583309a7bc5813cea4459dcd6c927eaaf2
Huy Pham <huypham0297@gmail.com>
2026-02-22 23:38:15 +0700

    feat: create DI container

---
+++ src/quantumgenerator/interfaces/api/dependencies.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from functools import lru_cache
+from quantumgenerator.infrastructure import (
+    ModelFactory,
+    RetrieverFactory,
+    SystemClock,
+)
+from quantumgenerator.application import (
+    GenerateQuantumCodeUseCase,
+    CodeGenerationService,
+)
+
+
+class DIContainer:
+    """Centralize dependency injection container."""
+
+    @lru_cache(maxsize=1)
+    def get_model_factory(self):
+        return ModelFactory()
+    
+    @lru_cache(maxsize=1)
+    def get_retriever_factory(self):
+        return RetrieverFactory()
+    
+    @lru_cache(maxsize=1)
+    def get_system_clock(self):
+        return SystemClock()
+
+    def get_code_generation_service(
+            self,
+            model_type: str = "codegemma", 
+            model_name: str = "google/codegemma-2b", 
+            retriever_type: str = "chroma"
+    ) -> CodeGenerationService:
+        generator = self.get_model_factory.create_model(
+            model_type, 
+            model_name=model_name
+        )
+        retriever = self.get_retriever_factory.create_retriever(retriever_type)
+        clock = self.get_system_clock()
+
+        use_case = GenerateQuantumCodeUseCase(generator, retriever)
+        return CodeGenerationService(use_case, clock)
+
+_container: Optional[DIContainer] = None
+def create_container() -> DIContainer:
+    global _container
+    if _container is None:
+        _container = DIContainer()
+    return _container
